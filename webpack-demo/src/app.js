377db84708619ea68d127f95967406df373ddf11
TianQianTQ <1114580464@qq.com>
2017-06-24 16:20:17 +0800

    添加babel-loader使用方法（转换ES6代码）

---
+++ webpack-demo/src/app.js
@@ -0,0 +1,8 @@
+import layer from './components/layer/layer.js'
+
+const App=function(){
+	var NUM=3;
+	alert(NUM);
+	console.log(layer);
+}
+new App();
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,66 +1,36 @@
-// 配置文件  执行webpack直接会在根目录寻找这个配置文件去执行
 
 var htmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 module.exports = {
-	//context:      //运行环境的上下文
+	context:__dirname,     //运行环境的上下文
 	//entry:'./src/script/main.js' ,  //打包的入口
 	// entry:['./src/script/main.js','./src/script/a.js'],
-	entry: {
-		main:'./src/script/main.js',
-		a:'./src/script/a.js',
-		b:'./src/script/b.js',
-		c:'./src/script/c.js'
-	},
+  entry:'./src/app.js',
 	output: {
-		path:__dirname+'/dist',   //打包以后的文件路径  绝对路径
-        // filename:'bundle.js'   //打包以后的文件名
-        filename:'js/[name].js' ,    //重新修改   因为有两个文件
-        //[name]-[hash].js    name+hash组合成文件名
-        publicPath: 'http://cdn.com/',              //占位符，把路径引用为线上地址
+		    path:__dirname+'/dist',   //打包以后的文件路径  绝对路径
+        filename:'js/[name].bundle.js' ,    //重新修改   因为有两个文件
 
 	},
+  module:{
+    rules:[                                 //es6
+      {
+        test:/\.js$/,
+        include:/("\.\/src")/,
+        exclude:path.resolve(__dirname,'/node_modules/'),
+        use:{
+          loader:'babel-loader',
+          options:{
+            presets:['env']
+          }
+        }
+      }
+    ]
+  },
 	plugins: [
         new htmlWebpackPlugin({
-        	// filename:'index-[hash].html',//指定文件名称,可当成版本号
-        	filename:'a.html',
+        	filename:'index.html',
         	template:'index.html',
-        	// inject:'head' ,   //指定放在头部还是body
-        	inject:false,
-        	// inject:'body',
-        	title:'webpack a',
-        	minify:{           //压缩
- 				removeComments:true,    //删除注释
- 				collapseInlineTagWhitespace:true,   //删除空格
-        	},
-        	// chunks: ['main','a'],
-        	excludeChunks: ['b','c'],    //指定除了哪些被排除之后其他的chunk
+        	inject:'body',
         }),
-         new htmlWebpackPlugin({
-        	// filename:'index-[hash].html',//指定文件名称,可当成版本号
-        	filename:'b.html',
-        	template:'index.html',
-        	// inject:'head' ,   //指定放在头部还是body
-        	inject:false,
-        	// inject:'body',
-        	title:'webpack b',
-        	// chunks: ['b'],
-        	excludeChunks: ['a','c'], 
-        }),
-         new htmlWebpackPlugin({
-        	// filename:'index-[hash].html',//指定文件名称,可当成版本号
-        	filename:'c.html',
-        	template:'index.html',
-        	// inject:'head' ,   //指定放在头部还是body
-        	inject:false,
-            // inject:'body',
-        	title:'webpack c',
-        	minify:{           //压缩
- 				removeComments:true,    //删除注释
- 				collapseInlineTagWhitespace:true,   //删除空格
-        	},
-        	// chunks: ['c'],
-        	excludeChunks: ['a','b'],
-        })
         ]
 }
